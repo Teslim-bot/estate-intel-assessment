aa727a61b2e968091e2ab4c62a4872bfda7fb27a
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2021-12-17 16:30:13 +0100

    Adds query

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <grid-selectors />
-    <grid-display />
+  <div
+    class="tw-container tw-mx-auto tw-py-4 tw-bg-neutral-300 tw-px-44 tw-mt-5"
+  >
+    <grid-selectors @update="updateQuery" />
+    <grid-display :filters="filters" />
   </div>
 </template>
 
@@ -11,5 +13,18 @@ import GridSelectors from "../components/gridSelectors.vue";
 export default {
   components: { gridDisplay, GridSelectors },
   name: "IndexPage",
+  data() {
+    return {
+      filters: {
+        color: [],
+        shape: [],
+      },
+    };
+  },
+  methods: {
+    updateQuery(filters) {
+      this.filters = filters;
+    },
+  },
 };
 </script>
