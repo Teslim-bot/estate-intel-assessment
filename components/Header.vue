0b35d1b9c21e3abb7263514f687b86789df207f0
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2021-12-17 21:16:50 +0100

    Adds header

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="tw-flex tw-justify-between">
+    <div>
+      <span>SHAPES</span>
+    </div>
+    <button class="tw-text-red-500">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,21 @@
 <template>
-  <div
-    class="tw-container tw-mx-auto tw-py-4 tw-bg-neutral-300 tw-px-7 lg:tw-px-44 tw-mt-5"
-  >
-    <grid-selectors @update="updateQuery" />
-    <grid-display :filters="filters" />
+  <div class="tw-container tw-mx-auto">
+    <app-header class="tw-px-14" />
+    <div
+      class="tw-py-4 tw-bg-neutral-300 tw-px-7 lg:tw-px-44 tw-mt-5"
+    >
+      <grid-selectors @update="updateQuery" />
+      <grid-display :filters="filters" />
+    </div>
   </div>
 </template>
 
 <script>
 import gridDisplay from "../components/gridDisplay.vue";
 import GridSelectors from "../components/gridSelectors.vue";
+import Header from "../components/Header.vue";
 export default {
-  components: { gridDisplay, GridSelectors },
+  components: { gridDisplay, GridSelectors, "app-header": Header },
   name: "IndexPage",
   data() {
     return {
